7b22551f9dd1400501000960920daeb53758015d
Kirill Kovalev <kir017@mail.ru>
2023-01-25 15:55:47 +0300

    написал код для решения задачи

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] FillArray()
+{
+  Console.WriteLine("Заполните массив (данные вводятся через пробел):  ");
+  return Console.ReadLine()!.Split(" ");
+}
+
+int GetValue(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountElemetNewArray(string[] text, int numberСharacters)
+{
+  int count = 0;
+  for (int i = 0; i < text.Length; i++)
+  {
+    if (text[i].Length <= numberСharacters)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+string[] CountLetter(string[] text, int lengthNewArray, int numberСharacters)
+{
+  string[] arrayLetter = new string[lengthNewArray];
+  for (int i = 0, j = 0; i < text.Length; i++)
+  {
+    if (text[i].Length <= numberСharacters)
+    {
+      arrayLetter[j] = text[i];
+      j++;
+    }
+  }
+  return arrayLetter;
+}
+
+string PrintArray(string[] prArray)
+{
+  string output = String.Empty;
+  for (int i = 0; i < prArray.Length; i++)
+  {
+    output = output + $"{prArray[i]} ";
+  }
+  return output;
+}
+
+string[] stringArray = FillArray();
+int countSymbols = GetValue("Введите максимальное количество символов строки в новом массиве (по условию задачи, 3): ");
+string[] newStringArray = CountLetter(stringArray, CountElemetNewArray(stringArray, countSymbols), countSymbols);
+string print = PrintArray(stringArray);
+string newPrint = PrintArray(newStringArray);
+Console.WriteLine($"\nЗаданный массива строк: [{print}] => новый массив из строк, длина которых меньше,"
+                + $" либо равна {countSymbols} символам:  [{newPrint}]\n");
